51d571bff2bdcd68ec1b86fae6ec27c0776ee311
nikole <nmmontealegre3@misena.edu.co>
2015-12-09 14:51:17 -0500

    CCCC

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': 'dakn4d8cucvak9',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'bwombpdmqmfdjd',
-        'PASSWORD': '',
+        'PASSWORD': 'aFpxz0JbNMNVNn2mumFNzIaDRT',
         'HOST': 'ec2-54-204-8-224.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
